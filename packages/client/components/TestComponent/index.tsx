79edb4f7f7af32597125c87e363da4154ab55f29
Mystaken <kgaotianx@gmail.com>
2019-10-11 18:02:48 -0400

    Added styled components example

--- packages/client/components/TestComponent/index.tsx
+++ packages/client/components/TestComponent/index.tsx
@@ -1,11 +1,21 @@
 import React, { FunctionComponent } from 'react'
+import styled from 'styled-components'
 
 export interface TestComponentProps {
   phrase: string
 }
 
-export const TestComponent: FunctionComponent<TestComponentProps> = ({
-  phrase
+const TestButton: FunctionComponent<TestComponentProps> = ({
+  phrase,
+  ...props
 }) => {
-  return <button>{phrase}</button>
+  return <button {...props}>{phrase}</button>
+}
+
+export const TestComponent: FunctionComponent<TestComponentProps> = props => {
+  // Using styled components
+  const StyledButton = styled(TestButton)`
+    background-color: lightblue;
+  `
+  return <StyledButton {...props}></StyledButton>
 }
