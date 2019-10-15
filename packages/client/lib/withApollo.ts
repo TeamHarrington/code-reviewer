1c70b42c60e2ef658d2096e2a74ffb360b79c15e
Mystaken <kgaotianx@gmail.com>
2019-10-15 16:51:11 -0400

    Added graphql in front-end

--- packages/client/lib/withApollo.ts
+++ packages/client/lib/withApollo.ts
@@ -1,11 +1,11 @@
 import withApollo from 'next-with-apollo'
 import ApolloClient, { InMemoryCache } from 'apollo-boost'
-import { config } from './config'
+import { config } from '@code-reviewer/client/config'
 
 export default withApollo(
   ({ initialState }) =>
     new ApolloClient({
-      uri: `${config.COMIC_SERVER_API}/${config.GRAPHQL_ENDPOINT}`,
+      uri: `${config.SERVER_API}/${config.GRAPHQL_ENDPOINT}`,
       cache: new InMemoryCache().restore(initialState || {})
     })
 )
