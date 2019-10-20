fce1c873d4966614a0df1c6a54aa60c7d37a20c5
Kevin Gao <kgaotianx@gmail.com>
2019-10-20 13:25:16 -0400

    Fix: Add graphql endpoint to client (#21)

--- packages/client/lib/withApollo.ts
+++ packages/client/lib/withApollo.ts
@@ -5,7 +5,7 @@ import config from '@code-reviewer/client/config'
 export default withApollo(
   ({ initialState }) =>
     new ApolloClient({
-      uri: config.SERVER_DOMAIN,
+      uri: `${config.SERVER_DOMAIN}/${config.GRAPHQL_ENDPOINT}`,
       cache: new InMemoryCache().restore(initialState || {})
     })
 )
