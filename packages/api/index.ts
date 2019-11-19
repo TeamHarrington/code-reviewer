4c071b4e44f7ac98b60c1683ab7cb32b3824b54b
Mystaken <kgaotianx@gmail.com>
2019-11-19 16:47:10 -0500

    Fix importing

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,16 +1,41 @@
-import ExpressApolloApp from './server'
+import ExpressServer, { Express } from 'express'
+import { ApolloServer, ApolloServerExpressConfig } from 'apollo-server-express'
+import GraphQLServer from './apollo-server'
+import Mongoose from 'mongoose'
 import config from './config'
 
-const { DOMAIN, PORT, GRAPHQL_ENDPOINT } = config.API
-const SERVER_DOMAIN = config.ENV === 'production' ? DOMAIN : `${DOMAIN}:${PORT}`
-
-const main = async () => {
-  const app = await ExpressApolloApp({
-    graphqlEndpoint: GRAPHQL_ENDPOINT
-  })
-  app.listen({ port: PORT }, () => {
-    console.log(`Server ready at ${SERVER_DOMAIN}`)
+const startMongoConnection = async () => {
+  return Mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
 }
 
-main()
+export interface ExpressAppConfig extends ApolloServerExpressConfig {
+  graphqlEndpoint: string
+}
+
+export interface ExpressApolloServer extends Express {
+  apolloServer: ApolloServer
+}
+
+const ExpressApolloApp = async ({
+  graphqlEndpoint,
+  ...apolloConfigs
+}: ExpressAppConfig): Promise<ExpressApolloServer> => {
+  // await startMongoConnection()
+  console.log(startMongoConnection)
+
+  const app = ExpressServer() as ExpressApolloServer
+  const apolloServer = GraphQLServer(apolloConfigs)
+  app.apolloServer = apolloServer
+
+  apolloServer.applyMiddleware({
+    app,
+    path: `/${graphqlEndpoint}`
+  })
+  return app
+}
+
+export { config }
+export default ExpressApolloApp
