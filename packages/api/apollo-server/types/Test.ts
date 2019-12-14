f135b9852543459e1a55e6deeef459929ede403b
BoZhaoUTSC <bodotzhao@gmail.com>
2019-12-13 21:39:50 -0500

    add types

--- packages/api/apollo-server/types/Test.ts
+++ packages/api/apollo-server/types/Test.ts
@@ -8,6 +8,47 @@ const Test = gql`
   type Mutation {
     testMutation: String
   }
+
+  enum UserType {
+    TA
+    STUDENT
+    INSTRUCTOR
+  }
+
+  type User {
+    firstName: String
+    lastName: String
+    utorID: String!
+    email: String
+    userType: UserType!
+    isActive: Boolean
+  }
+
+  type Assignment {
+    name: String
+    requiredFiles: [String]
+    peerReviewDeadline: String
+    feedbackQuestions: [String]
+    numPeers: Int
+  }
+
+  type File {
+    name: String
+    path: String
+    annotations: Annotation
+  }
+
+  type Submission {
+    author: User
+    assignment: Assignment
+    files: [File]
+  }
+
+  type Annotation {
+    givenBy: User
+    line: Int
+    content: String
+  }
 `
 
 export default Test
