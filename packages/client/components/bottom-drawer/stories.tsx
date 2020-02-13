3908507fdc06ba2b7f03b21f96b07f6960319c46
BoZhaoUTSC <bodotzhao@gmail.com>
2020-02-12 21:38:12 -0500

    add bottom drawer

--- packages/client/components/bottom-drawer/stories.tsx
+++ packages/client/components/bottom-drawer/stories.tsx
@@ -13,12 +13,6 @@ const stories = storiesOf('Bottom Drawer', module)
 stories.addParameters({ info: { inline: true } }).addDecorator(withKnobs)
 
 const StoryContainer = styled.div`
-  position: fixed;
-  bottom: 0px;
-  width: 100%;
-`
-
-const Background = styled.div`
   background-color: #ddd;
   position: fixed;
   top: 0px;
@@ -29,96 +23,90 @@ const Background = styled.div`
 
 stories.add('default', () => {
   return (
-    <Background>
-      <StoryContainer>
-        <BottomDrawer
-          title={text('title', 'Drawer Title')}
-          actionButton={<Button color="primary">Button</Button>}>
-          some drawer content
-        </BottomDrawer>
-      </StoryContainer>
-    </Background>
+    <StoryContainer>
+      <BottomDrawer
+        title={text('title', 'Drawer Title')}
+        actionButton={<Button color="primary">Button</Button>}>
+        some drawer content
+      </BottomDrawer>
+    </StoryContainer>
   )
 })
 
 stories.add('with questions and answer', () => {
   return (
-    <Background>
-      <StoryContainer>
-        <BottomDrawer
-          title={text('title', 'Questions')}
-          actionButton={
-            <Button
-              onClick={action('button clicked')}
-              disabled={boolean('disable save button', false)}
-              color="primary">
-              Save
-            </Button>
-          }>
-          <>
-            <TextQuestionAnswer
-              editable
-              index={1}
-              question={
-                'Did the author use meaningful and descriptive variable names?'
-              }
-              answers={[
-                'Mostly yes, except for a few places the author used “xxx”. I think “yyy” would be more clear.'
-              ]}
-            />
-            <TextQuestionAnswer
-              editable
-              index={2}
-              question={'Is the algorithm in function xxx efficient?'}
-              answers={[
-                'I think so. An alternative (equally efficient) way would be to xxx.'
-              ]}
-            />
-            <TextQuestionAnswer
-              editable
-              index={3}
-              question={'Is the algorithm in function xxx efficient?'}
-              answers={[
-                'I think so. An alternative (equally efficient) way would be to xxx.'
-              ]}
-            />
-            <TextQuestionAnswer
-              editable
-              index={4}
-              question={'Is the algorithm in function xxx efficient?'}
-              answers={[
-                'I think so. An alternative (equally efficient) way would be to xxx.'
-              ]}
-            />
-          </>
-        </BottomDrawer>
-      </StoryContainer>
-    </Background>
+    <StoryContainer>
+      <BottomDrawer
+        title={text('title', 'Questions')}
+        actionButton={
+          <Button
+            onClick={action('button clicked')}
+            disabled={boolean('disable save button', false)}
+            color="primary">
+            Save
+          </Button>
+        }>
+        <>
+          <TextQuestionAnswer
+            editable
+            index={1}
+            question={
+              'Did the author use meaningful and descriptive variable names?'
+            }
+            answers={[
+              'Mostly yes, except for a few places the author used “xxx”. I think “yyy” would be more clear.'
+            ]}
+          />
+          <TextQuestionAnswer
+            editable
+            index={2}
+            question={'Is the algorithm in function xxx efficient?'}
+            answers={[
+              'I think so. An alternative (equally efficient) way would be to xxx.'
+            ]}
+          />
+          <TextQuestionAnswer
+            editable
+            index={3}
+            question={'Is the algorithm in function xxx efficient?'}
+            answers={[
+              'I think so. An alternative (equally efficient) way would be to xxx.'
+            ]}
+          />
+          <TextQuestionAnswer
+            editable
+            index={4}
+            question={'Is the algorithm in function xxx efficient?'}
+            answers={[
+              'I think so. An alternative (equally efficient) way would be to xxx.'
+            ]}
+          />
+        </>
+      </BottomDrawer>
+    </StoryContainer>
   )
 })
 
 stories.add('with annotations', () => {
   return (
-    <Background>
-      <StoryContainer>
-        <BottomDrawer
-          fixedHeight="178px"
-          title={text('title', 'Annotations')}
-          actionButton={
-            <Button onClick={action('button clicked')}>
-              <CloseIcon />
-            </Button>
-          }>
-          <Annotations
-            lineNum={number('first line number', 7)}
-            annotations={[
-              {
-                content: text('first annotation', 'What is this?')
-              }
-            ]}
-          />
-        </BottomDrawer>
-      </StoryContainer>
-    </Background>
+    <StoryContainer>
+      <BottomDrawer
+        fixedHeight="178px"
+        title={text('title', 'Annotations')}
+        actionButton={
+          <Button onClick={action('button clicked')}>
+            <CloseIcon />
+          </Button>
+        }>
+        <Annotations
+          lineNum={number('first line number', 7)}
+          annotations={[
+            {
+              content: text('first annotation', 'What is this?')
+            }
+          ]}
+        />
+      </BottomDrawer>
+    </StoryContainer>
   )
 })
