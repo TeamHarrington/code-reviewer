968fa4ddcad50f0dfa057822127d6197afd33301
BoZhaoUTSC <bodotzhao@gmail.com>
2020-02-23 20:32:40 -0500

    add on hover

--- packages/client/components/syntax-highlight/index.tsx
+++ packages/client/components/syntax-highlight/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { vs2015 } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 
@@ -15,22 +15,39 @@ export const SyntaxHighlight = ({
   highlightedLines = [],
   language = 'python'
 }: Props) => {
+  const [currentLineNumber, setCurrentLineNumber] = useState(-1)
+
   return (
-    <SyntaxHighlighter
-      customStyle={{ fontSize: '20px', marginTop: 0 }}
-      showLineNumbers
-      style={colorTheme}
-      wrapLines
-      language={language}
-      lineProps={(lineNumber: number) => {
-        const style: any = {}
-        if (highlightedLines.includes(lineNumber)) {
-          style['backgroundColor'] =
-            colorTheme['hljs-addition'].backgroundColor || 'yellow'
-        }
-        return { style } as React.DOMAttributes<HTMLElement>
-      }}>
-      {codeString}
-    </SyntaxHighlighter>
+    <div>
+      <div>{`currentLineNumber: ${currentLineNumber}`}</div>
+      <SyntaxHighlighter
+        customStyle={{ fontSize: '20px', marginTop: 0 }}
+        showLineNumbers
+        style={colorTheme}
+        wrapLines
+        language={language}
+        lineProps={(lineNumber: number) => {
+          const style: any = {}
+          if (highlightedLines.includes(lineNumber)) {
+            style['backgroundColor'] =
+              colorTheme['hljs-addition'].backgroundColor || 'yellow'
+          }
+          if (currentLineNumber === lineNumber) {
+            style['opacity'] = '50%'
+          }
+
+          return {
+            style,
+            onMouseEnter() {
+              setCurrentLineNumber(lineNumber)
+            },
+            onMouseLeave() {
+              setCurrentLineNumber(-1)
+            }
+          } as React.DOMAttributes<HTMLElement>
+        }}>
+        {codeString}
+      </SyntaxHighlighter>
+    </div>
   )
 }
