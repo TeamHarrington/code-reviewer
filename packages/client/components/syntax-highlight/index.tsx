6b9317b3639336a1ebcd692d2504c559cb2339d8
BoZhaoUTSC <bodotzhao@gmail.com>
2020-02-27 00:16:52 -0500

    add background

--- packages/client/components/syntax-highlight/index.tsx
+++ packages/client/components/syntax-highlight/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { vs2015 } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+import { Grid } from '@material-ui/core'
+import styled from 'styled-components'
 
 export interface Props {
   codeString: string
@@ -8,6 +10,17 @@ export interface Props {
   colorTheme?: any
   language?: string
 }
+
+const Container = styled(Grid)``
+
+const LeftContainer = styled(Grid)<{ backgroundColor: string }>`
+  width: 32px;
+  background: #1e1e1e;
+`
+
+const CodeContainer = styled(Grid)`
+  flex: 1;
+`
 
 export const SyntaxHighlight = ({
   codeString,
@@ -18,36 +31,41 @@ export const SyntaxHighlight = ({
   const [currentLineNumber, setCurrentLineNumber] = useState(-1)
 
   return (
-    <div>
-      <div>{`currentLineNumber: ${currentLineNumber}`}</div>
-      <SyntaxHighlighter
-        customStyle={{ fontSize: '20px', marginTop: 0 }}
-        showLineNumbers
-        style={colorTheme}
-        wrapLines
-        language={language}
-        lineProps={(lineNumber: number) => {
-          const style: any = {}
-          if (highlightedLines.includes(lineNumber)) {
-            style['backgroundColor'] =
-              colorTheme['hljs-addition'].backgroundColor || 'yellow'
-          }
-          if (currentLineNumber === lineNumber) {
-            style['opacity'] = '50%'
-          }
-
-          return {
-            style,
-            onMouseEnter() {
-              setCurrentLineNumber(lineNumber)
-            },
-            onMouseLeave() {
-              setCurrentLineNumber(-1)
+    <Container container direction="row" id="ghjk">
+      <LeftContainer backgroundColor={colorTheme['hljs'].background}>
+        {`currentLineNumber: ${currentLineNumber}`}
+      </LeftContainer>
+      <CodeContainer>
+        <SyntaxHighlighter
+          customStyle={{ fontSize: '20px', marginTop: 0 }}
+          showLineNumbers
+          style={colorTheme}
+          wrapLines
+          language={language}
+          lineProps={(lineNumber: number) => {
+            const style: any = {}
+            if (highlightedLines.includes(lineNumber)) {
+              style['backgroundColor'] =
+                colorTheme['hljs-addition'].backgroundColor || 'yellow'
             }
-          } as React.DOMAttributes<HTMLElement>
-        }}>
-        {codeString}
-      </SyntaxHighlighter>
-    </div>
+            // TBD: hover effect
+            if (currentLineNumber === lineNumber) {
+              style['opacity'] = '50%'
+            }
+
+            return {
+              style,
+              onMouseEnter() {
+                setCurrentLineNumber(lineNumber)
+              },
+              onMouseLeave() {
+                setCurrentLineNumber(-1)
+              }
+            } as React.DOMAttributes<HTMLElement>
+          }}>
+          {codeString}
+        </SyntaxHighlighter>
+      </CodeContainer>
+    </Container>
   )
 }
