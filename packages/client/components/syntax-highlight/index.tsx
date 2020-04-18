18d787dc54c0b2a2636d410faa0bc2fa22299ae1
BoZhaoUTSC <bodotzhao@gmail.com>
2020-04-18 11:38:08 -0400

    remove annotation indicator

--- packages/client/components/syntax-highlight/index.tsx
+++ packages/client/components/syntax-highlight/index.tsx
@@ -1,87 +1,36 @@
-import React, { useState } from 'react'
+import React from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { vs2015 } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
-import { Grid } from '@material-ui/core'
-import styled from 'styled-components'
-import { AnnotationIndicators } from '../annotation-indicators'
 
-export interface SyntaxHighlightProps {
+export interface Props {
   codeString: string
   highlightedLines?: number[]
   colorTheme?: any
   language?: string
-  editable?: boolean
-  annotations?: any[]
 }
-
-const Container = styled(Grid)``
-
-const LeftContainer = styled(Grid)`
-  width: 32px;
-`
-
-const CodeContainer = styled(Grid)`
-  flex: 1;
-`
 
 export const SyntaxHighlight = ({
   codeString,
   colorTheme = vs2015,
   highlightedLines = [],
-  language = 'python',
-  editable = false,
-  annotations = []
-}: SyntaxHighlightProps) => {
-  const [currentLineNumber, setCurrentLineNumber] = useState(-1)
-  const [
-    selectedAnnotationLineNumber,
-    setSelectedAnnotationLineNumber
-  ] = useState(-1)
-
+  language = 'python'
+}: Props) => {
   return (
-    <Container container>
-      <LeftContainer>
-        <AnnotationIndicators
-          editable={editable}
-          totalLines={10}
-          annotations={annotations}
-          currentLineNumber={currentLineNumber}
-          setCurrentLineNumber={setCurrentLineNumber}
-          selectedAnnotationLineNumber={selectedAnnotationLineNumber}
-          setSelectedAnnotationLineNumber={setSelectedAnnotationLineNumber}
-        />
-      </LeftContainer>
-      <CodeContainer>
-        <SyntaxHighlighter
-          customStyle={{ fontSize: '20px', margin: 0 }}
-          showLineNumbers
-          style={colorTheme}
-          wrapLines
-          language={language}
-          lineProps={(lineNumber: number) => {
-            const style: any = {}
-            if (highlightedLines.includes(lineNumber)) {
-              style['backgroundColor'] =
-                colorTheme['hljs-addition'].backgroundColor || 'yellow'
-            }
-            // TBD: hover effect
-            if (currentLineNumber === lineNumber) {
-              style['opacity'] = '50%'
-            }
-
-            return {
-              style,
-              onMouseEnter() {
-                setCurrentLineNumber(lineNumber)
-              },
-              onMouseLeave() {
-                setCurrentLineNumber(-1)
-              }
-            } as React.DOMAttributes<HTMLElement>
-          }}>
-          {codeString}
-        </SyntaxHighlighter>
-      </CodeContainer>
-    </Container>
+    <SyntaxHighlighter
+      customStyle={{ fontSize: '20px' }}
+      showLineNumbers
+      style={colorTheme}
+      wrapLines
+      language={language}
+      lineProps={(lineNumber: number) => {
+        const style: any = {}
+        if (highlightedLines.includes(lineNumber)) {
+          style['backgroundColor'] =
+            colorTheme['hljs-addition'].backgroundColor || 'yellow'
+        }
+        return { style } as React.DOMAttributes<HTMLElement>
+      }}>
+      {codeString}
+    </SyntaxHighlighter>
   )
 }
--- packages/client/components/syntax-highlight/stories.tsx
+++ packages/client/components/syntax-highlight/stories.tsx
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/react'
 import { SyntaxHighlight } from '.'
-import { withKnobs, text, select, boolean } from '@storybook/addon-knobs'
+import { withKnobs, text, select } from '@storybook/addon-knobs'
 import * as colorThemes from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 
 const stories = storiesOf('Syntax Highlight', module)
@@ -10,13 +10,7 @@ stories.addParameters({ info: { inline: true } }).addDecorator(withKnobs)
 stories.add('default', () => {
   const codeString = `def myfunc():
   result = ["str", True, 1, []]
-  return result
-
-
-
-
-
-  `
+  return result`
 
   const colorThemeOptions = {
     ...colorThemes
@@ -28,18 +22,17 @@ stories.add('default', () => {
     colorThemes.vs2015
   )
 
-  const annotations = [{ lineNumber: 0 }, { lineNumber: 2 }]
-
   return (
     <div>
-      <SyntaxHighlight
-        language={text('language', 'python')}
-        colorTheme={colorTheme}
-        codeString={codeString}
-        highlightedLines={[1]}
-        annotations={annotations}
-        editable={boolean('editable', true)}
-      />
+      <div>
+        <SyntaxHighlight
+          language={text('language', 'python')}
+          colorTheme={colorTheme}
+          codeString={codeString}
+          highlightedLines={[1]}
+        />
+      </div>
+      <div></div>
     </div>
   )
 })
