3a1030b553119760c310d831305ef42bedabf37e
BoZhaoUTSC <bodotzhao@gmail.com>
2020-05-03 21:19:13 -0400

    remove snapshot test

--- packages/client/components/syntax-highlight/test.tsx
+++
@@ -1,9 +0,0 @@
-import { SyntaxHighlight } from '.'
-import renderer from 'react-test-renderer'
-import React from 'react'
-
-test('Syntax Highlight Component', () => {
-  const component = renderer.create(<SyntaxHighlight codeString="" />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
