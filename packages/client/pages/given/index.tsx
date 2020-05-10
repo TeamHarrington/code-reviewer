dc3114af5928b48519f4646ea8deda2a1bb93448
BoZhaoUTSC <bodotzhao@gmail.com>
2020-05-10 19:48:12 -0400

    use feedback drawer

--- packages/client/pages/given/index.tsx
+++ packages/client/pages/given/index.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from 'react'
 import { PeerTabs } from '../../components/peer-tabs'
 import { Header } from '../../components/header'
 import { SyntaxHighlight } from '../../components/syntax-highlight'
-import { BottomDrawer } from '../../components/bottom-drawer'
+import { FeedbackDrawer } from '../../components/bottom-drawer'
 import { TextQuestionAnswer } from '../../components/text-question-answer'
 import { Button } from '@material-ui/core'
 
@@ -10,117 +10,6 @@ const HomePage: FunctionComponent = () => {
   const codeString = `def myfunc():
   result = ["str", True, 1, []]
   return result
-
-
-
-  result = ["str", True, 1, []]
-  return result
-  result = ["str", True, 1, []]
-  return result
-  result = ["str", True, 1, []]
-  return result
-  result = ["str", True, 1, []]
-  return result
-  result = ["str", True, 1, []]
-  return result
-  result = ["str", True, 1, []]
-  return result
-  result = ["str", True, 1, []]
-  return result
-  result = ["str", True, 1, []]
-  return result
-  result = ["str", True, 1, []]
-  return result
-  result = ["str", True, 1, []]
-  return result
-
-
-  result = ["str", True, 1, []]
-  return result
-  result = ["str", True, 1, []]
-  return result
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  result = ["str", True, 1, []]
-  return result
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   `
   const saveButton = (
@@ -134,26 +23,18 @@ const HomePage: FunctionComponent = () => {
     <>
       <Header title={'CSCA20'} userName={'Kenny'} />
       <PeerTabs contents={[peer1, 'Peer 2', 'Peer 3']} />
-      <BottomDrawer title={'Questions'} actionButton={saveButton}>
-        <TextQuestionAnswer
-          editable
-          index={1}
-          question={
-            'Did the author use meaningful and descriptive variable names?'
-          }
-          answer={
-            'Mostly yes, except for a few places the author used “xxx”. I think “yyy” would be more clear.'
-          }
-        />
-        <TextQuestionAnswer
-          editable
-          index={2}
-          question={'Is the algorithm in funciton xxx sufficient?'}
-          answer={
-            'I think so. An alternative (equally efficient way) would be xxx.'
-          }
-        />
-      </BottomDrawer>
+
+      <FeedbackDrawer
+        editable
+        questions={[
+          'Did the author use meaningful and descriptive variable names?',
+          'Is the algorithm in function xxx efficient?'
+        ]}
+        answers={[
+          'Mostly yes, except for a few places the author used “xxx”. I think “yyy” would be more clear.',
+          'I think so. An alternative (equally efficient) way would be to xxx.'
+        ]}
+      />
     </>
   )
 }
