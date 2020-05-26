bb991eb2b3390ad14f6d3a1665813f16d0757840
lilyren8 <lilyren8@gmail.com>
2020-05-25 22:25:46 -0400

    fix

--- packages/client/components/assignment-selections/index.tsx
+++ packages/client/components/assignment-selections/index.tsx
@@ -9,7 +9,6 @@ import {
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import styled from 'styled-components'
 import Link from 'next/link'
-import { LeftDrawer } from '../drawers'
 
 const TitleText = styled.div`
   font-size: 20px;
