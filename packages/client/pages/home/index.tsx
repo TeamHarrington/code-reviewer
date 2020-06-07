dae5cfe13bf115d69b3d55a5d4d87f7c8d0a4df9
BoZhaoUTSC <bodotzhao@gmail.com>
2020-06-07 00:01:29 -0400

    add a new home page

---
+++ packages/client/pages/home/index.tsx
@@ -0,0 +1,16 @@
+import { PeerTabs } from '../../components/peer-tabs'
+import { Header } from '../../components/header'
+import styled from 'styled-components'
+
+const PageContainer = styled.div``
+
+const HomePage = () => {
+  return (
+    <PageContainer>
+      <Header title={'CSCA20'} userName={'Kenny'} />
+      home Page
+    </PageContainer>
+  )
+}
+
+export default HomePage
--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,15 +1,10 @@
 // import { Query } from '../components/apollo/query'
-// import { TestQuery } from '@code-reviewer/client/graphql/types'
-// import { testQuery } from '@code-reviewer/client/graphql/queries/testQuery'
+// import { TestQuery } from '../graphql/types'
+// import { testQuery } from '../graphql/queries/testQuery'
 
 const HomePage = () => {
   return (
-    <div> home page</div>
-    // <Query<TestQuery> query={testQuery}>
-    //   {data => {
-    //     return <div>{data.testQuery}</div>
-    //   }}
-    // </Query>
+    <div>root page</div>
   )
 }
 
