acda4a603b3f479a539bd44efd89283e0cdee193
BoZhaoUTSC <bodotzhao@gmail.com>
2020-07-19 03:46:12 -0400

    update

--- packages/client/pages/home/index.tsx
+++ packages/client/pages/home/index.tsx
@@ -3,24 +3,18 @@ import styled from 'styled-components'
 import { useQuery } from '@apollo/react-hooks'
 import { GET_ASSIGNMENTS } from '../../graphql/queries'
 import { AssignmentSelection } from '../../components/assignment-selections'
-import { format, parse } from 'date-fns'
 
 const PageContainer = styled.div``
 
 const HomePage = () => {
   // get all assignments
-  const { data: assignmentData, loading: isAssignmentsLoading } = useQuery(
-    GET_ASSIGNMENTS
-  )
-  console.log('===== assignment data', assignmentData)
+  const { data, loading } = useQuery(GET_ASSIGNMENTS)
 
   return (
     <PageContainer>
       <Header title={'CSCA20'} userName={'Kenny'} />
-      {isAssignmentsLoading && <p>Loading Assignments</p>}
-      {!isAssignmentsLoading && (
-        <AssignmentSelection assignments={assignmentData.getAssignments} />
-      )}
+      {loading && <p>Loading Assignments</p>}
+      {!loading && <AssignmentSelection assignments={data.getAssignments} />}
     </PageContainer>
   )
 }
